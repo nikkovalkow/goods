3a3c1b00f81d7974e6e1cc540b81199c9a9f360b
Mikita Kavalkou <nikkovalkow@yandex.ru>
2018-12-10 17:34:51 +0300

    GetPageText function with exception

--- onliner-get-database.py
+++ onliner-get-database.py
@@ -1,7 +1,31 @@
+from urllib.request import Request, urlopen
+import urllib.error
+
+def GetPageText(url):
+
+# Gets URL as text, return [Text,Info],
+# in case of HTML error returns [Ecode,Ecode]
+# in case of non-HTML error retuens [None,None] 
+
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+    try:
+        response=urlopen(req)
+    except urllib.error.HTTPError as e:  
+        return [e.code,e.code]
+    except:
+        return [None,None]
+    
+    
+    data = response.read()
+    info=response.info()
+    
+    return [data,info]
+    
+    
+        
+    
+print (GetPageText('https://by.ebay.com/b/Cell-Phones-Smartphones/9355/bn_320094')[0])
 
 
-print ('database')
-print ("new commit")
 
-#comment n1
-#one more coment1asas
