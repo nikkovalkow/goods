cc17408a58aa8544281752489f3e2ad137e2d280
Mikita Kavalkou <nikkovalkow@yandex.ru>
2019-01-17 11:38:36 +0300

    error trying, code for all pages analyse

--- onliner/get-content-kufar.py
+++ onliner/get-content-kufar.py
@@ -11,14 +11,32 @@ import time
 import pprint
 
 import demjson
+import time
+
 
 def DBPutLogMessage(message):
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["onliner"]
-    mycol = mydb["log"]
+    try:
+        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+        mydb = myclient["kufar"]
+        mycol = mydb["log"]
+        
+        msg={"time":datetime.datetime.now(),"msg":message}
+        mycol.insert_one(msg)
+    except:
+        print("DBPutLogMessage() DB open error  "+str(datetime.datetime.now()))
+
+def DBPutObject(db_client,db_name, collection_name, dict_obj):
+
+    try:
+        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+        mydb = myclient[sb_name]
+        mycol = mydb[collection_name]
+        mycol.insert_one(dict_obj)
+    except:
+        print("DBPutObject() DB open error" + str(datetime.datetime.now()))
+        
     
-    msg={"time":datetime.datetime.now(),"msg":message}
-    mycol.insert_one(msg)
+    
 
 
 def GetPageText(url):
@@ -56,46 +74,73 @@ def ExceptionMessage(command):
     DBPutLogMessage(command)
     
     
+
 def GetKufarAdList(page_text):
+    #puts information from one of the kufar googs list page into DICT
+    #input - text of listing page
+    #outpout - list of dicts, each dict is one AD on listing page
     page=html.document_fromstring(page_text)
+    
     AdList=page.find_class("list_ads__title")
     x=0
+    resultList=[]
     for i in AdList:
-        print(i.text_content())
-        print(i.get("href"))
-        text=GetPageText(quote(i.get("href"),safe="%/:=&?~#+!$,;'@()*[]"))
+        try:         
+            #extracting JajaScriptObject from page
+            text=GetPageText(quote(i.get("href"),safe="%/:=&?~#+!$,;'@()*[]"))
+            text=text[text.find('function pulseTrackPhoneNumberDisplayed(event)'):text.find('function pulseTrackAdReplySubmitted')]
+            text=text[text.find('object'):text.find('});')+1]              
+            
+            #Converting JS object to DICT
+            ADdict=demjson.decode("{"+text)
+            
+            #restructurizing DICT to one level
+            del ADdict['origin']
+            del ADdict['name']
+            del ADdict['provider']
+            del ADdict['type']
+            del ADdict['deployStage']
+            del ADdict['deployTag']
+            ADdict['object']['inReplyTo']['cust_name']=ADdict['object']['name']
+            ADdict['object']['inReplyTo']['phone']=ADdict['object']['telephone']
+            ADdict=ADdict['object']['inReplyTo']
+            ADdict['Region']=ADdict['location']['addressRegion']
+            ADdict['Subarea']=ADdict['location']['addressSubarea']
+            ADdict['href']=i.get("href")
+            ADdict['title']=i.text_content()
+            del ADdict['location']
+            resultList.append(ADdict)
+        except:
+            DBPutLogMessage("GetKufarADList() AD add failed link:" - i.get("href"))
+            
+            
+    return resultList    
+            
         
-        text=text[text.find('function pulseTrackPhoneNumberDisplayed(event)'):text.find('function pulseTrackAdReplySubmitted')]
-        text=text[text.find('object'):text.find(');')]
         
-        
-        ADdict=demjson.decode("{"+text)
         
 
-        del ADdict['origin']
-        del ADdict['name']
-        del ADdict['provider']
-        del ADdict['type']
-        del ADdict['deployStage']
-        del ADdict['deployTag']
-        ADdict['object']['inReplyTo']['cust_name']=ADdict['object']['name']
-        ADdict['object']['inReplyTo']['phone']=ADdict['object']['telephone']
-        ADdict=ADdict['object']['inReplyTo']
-        ADdict['Region']=ADdict['location']['addressRegion']
-        ADdict['Subarea']=ADdict['location']['addressSubarea']
-        del ADdict['location']
-        
-        
-        pprint.pprint(ADdict)
-        
-        
-        
-        
 
-page=GetPageText("https://www.kufar.by/"+quote('минск_город/Телефоны')+'?cu=BYR&phce=1&o=100')
+for pageNum in range (0,10000):
+    
+    page=GetPageText("https://www.kufar.by/"+quote('минск_город/Телефоны')+'?cu=BYR&phce=1&o='+str(pageNum))
+
+    if page.find('поиск расширен на всю страну')==-1:
+        t1 = time.perf_counter()
+
+        resultList=GetKufarAdList(page)
+
+        t2 = time.perf_counter()
+
+        print(t2-t1)
+        print((t2-t1)/len(resultList))
+        
+    else:
+        result={''}
+        DBPutObject
+        break
 
 
-GetKufarAdList(page)
 
 
 
